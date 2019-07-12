9d97c058831df4e118f053d794cb982131e3a31f
Kristián Feldsam <feldsam@gmail.com>
2019-07-12 11:33:30 +0200

    Set version to 5.8.3.2
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -50,7 +50,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.8.3'
+    VERSION = '5.8.3.2'
 
     # #########################################################################
     # Default location for the authentication file
