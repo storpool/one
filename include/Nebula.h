e39219b3be76529aceced8337a1525040d363fed
Kristián Feldsam <feldsam@gmail.com>
2019-10-03 22:41:05 +0200

    Bump version to 5.8.5.3
    Signed-off-by: Kristián Feldsam <feldsam@gmail.com>

--- include/Nebula.h
+++ include/Nebula.h
@@ -362,7 +362,7 @@ public:
      */
     static string code_version()
     {
-        return "5.8.5.2"; // bump version
+        return "5.8.5.3"; // bump version
     }
 
     /**
--- src/cloud/common/CloudClient.rb
+++ src/cloud/common/CloudClient.rb
@@ -50,7 +50,7 @@ end
 module CloudClient
 
     # OpenNebula version
-    VERSION = '5.8.5.2'
+    VERSION = '5.8.5.3'
 
     # #########################################################################
     # Default location for the authentication file
